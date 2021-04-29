c9b6552a7d913f6236e37ac49bfbe505b55bb432
Eirik Kvistad <eirik.kvistad@gmail.com>
2021-04-29 19:14:27 +0200

    first commit

---
+++ mockdata.py
@@ -0,0 +1,58 @@
+# list of student data, proto 2, 1 itt
+colsStud = ['Bruker','Opg1','Opg2','Opg3','Opg4','Totaltid','Antall feiltrykk','Hvor h*n trengte hjelp']
+dataStud1 = [
+    [1,152,107,37,21,317,6, ['Opg2']],
+    [2,83,80,30,120,313,7,['Opg1', 'Opg2']],
+    [3,10,80,30,10,130,2, ['Opg2', 'Opg3']],
+    [4,35,198,20,131,384,9,['Opg4']],
+    [5,50,59,98,123,330,5,[]]
+]
+# list of student data, proto 2, 2 itt
+dataStud2 = [
+    [1,20,29,20,10,79,2,[]],
+    [2,22,10,20,35,87,2,['Opg3']],
+    [3,17,10,16,9,52,0,[]],
+    [4,21,19,12,10,62,0,[]],
+    [5,19,15,22,26,82,0, []]
+]
+
+# list of company data, proto 2, 1 itt
+colsBed= ["Bruker","Opg5","Opg6","Totaltid","Antall feiltrykk", 'Hvor h*n trengte hjelp']
+
+databed1 = [
+    [1,181,78,259,0,[]],
+    [2,55,40,95,1,[]],
+    [3,73,14,87,0,[]],
+    [4,53,25,78,0,[]],
+    [5,67,50,117,0,[]],
+]
+
+databed2 = [
+    [1,46,26,72,0,[]],
+    [2,5,15,20,0,[]],
+    [3,50,22,72,1,[]],
+    [4,30,20,50,0,[]],
+    [5,57,21,78,1,[]],
+]
+
+
+# list of admin data, proto 2, 1 itt
+colsAdmin= ["Bruker","Opg7","Opg8","Totaltid","Antall feiltrykk", 'Hvor h*n trengte hjelp']
+
+dataAdmin1 = [
+    [1,108,55,163,0,[]],
+    [2,16,54,70,2,["Opg8"]],
+    [3,20,18,38,0,[]],
+    [4,20,35,55,1,[]],
+    [5,38,42,80,1,[]],
+]
+
+dataAdmin2 = [
+
+    [1,10,14,24,0,[]],
+    [2,10,20,30,0,[]],
+    [3,18,30,48,1,[]],
+    [4,15,21,36,0,[]],
+    [5,23,35,58,1,["Opg8"]],
+]
+
